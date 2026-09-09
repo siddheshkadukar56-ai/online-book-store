b19efc41d73cd91c933b9f7476ba40fbf24859d3
siddheshkadukar56-ai <siddheshkadukar56@gmail.com>
2026-09-09 17:16:59 +0530

    Update App.java

--- src/main/java/com/bookstore/App.java
+++ src/main/java/com/bookstore/App.java
@@ -1,1 +1,12 @@
+package com.bookstore;
 
+public class App {
+
+    public static String getMessage() {
+        return "Online Book Store";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
